5ef14f0bb5e0f85150b576520af31f1e260bc66b
mohamed el gharbaoui <mohamed01gharbaoui@gmail.com>
2024-11-21 11:46:54 +0100

    fix setup for autocompletion for clangd

--- setup.sh
+++ setup.sh
@@ -1,2 +1,4 @@
+sed "s|/Users/mohamed/workspace/stm32_projects/tests/libopencm3_empty_setup|$(pwd)|g" compile_commands.json > tmp.json
+mv tmp.json compile_commands.json
 git submodule update --init --recursive
 cd external/libopencm3 && make TARGETS='stm32/f4'
